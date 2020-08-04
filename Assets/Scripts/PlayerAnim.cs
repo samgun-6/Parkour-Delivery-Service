aa812996926f219ba7bba0d1096f3d497c9d4b4a
samgun-6 <samuel.gunnarsson@gmail.com>
2020-08-04 04:48:45 +0200

    Organizing assets folder. add player animations for running. Flip the player when facing opposite direction

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour {
+
+    private Animator anim;
+    private PlayerController PController;
+    void Start() {
+        anim = GetComponent<Animator>();
+        PController = GetComponent<PlayerController>();
+    }
+
+    void Update() {
+        if (Input.GetButton("Horizontal") && PController.checkIfGrounded()) {
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour{
     public float speed;
     public float elasticity;
     private Rigidbody2D rigBody;
+    private bool facingRight;
+
+    //Ground variables
     bool isGrounded = false;
     public Transform isGroundedChecker;
     public float checkGroundRadius;
@@ -22,20 +25,30 @@ public class PlayerController : MonoBehaviour{
     
     void Start(){
         rigBody = GetComponent<Rigidbody2D>();  //Get the rigidbody component
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update(){
-        Move();
+        float horizontal = Input.GetAxis("Horizontal");
+        Move(horizontal);
+        flip(horizontal);
         Jump();
-        checkIfGrounded();
+        isGrounded = checkIfGrounded();
         BetterJump();
     }
 
-    void Move() {
-        float x = Input.GetAxis("Horizontal");
-        float movement = x * speed;           //Decides movement speed per second
+    void Move(float horizontal) {
+        float movement = horizontal * speed;           //Decides movement speed per second
         rigBody.velocity = new Vector2(movement, rigBody.velocity.y);
+    }
+    void flip(float horizontal) {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+            facingRight = !facingRight;
+            Vector3 playerScale = transform.localScale;
+            playerScale.x *= -1;
+            transform.localScale = playerScale;
+        }
     }
     void Jump() {
         if (Input.GetButtonDown("Jump") && isGrounded) {
@@ -56,12 +69,12 @@ public class PlayerController : MonoBehaviour{
         }
     }
 
-    void checkIfGrounded() {
+    public bool checkIfGrounded() {
         Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
         if(collider != null) {
-            isGrounded = true;
+            return true;
         } else {
-            isGrounded = false;
+            return false;
         }
     }
 }
